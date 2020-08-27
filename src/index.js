7b10a060bcc68f6a26c2173e79ff55b553bc5bfb
harish <harishvm003@gmail.com>
2020-08-27 17:36:15 +0530

    First Application

--- src/index.js
+++ src/index.js
@@ -4,10 +4,63 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+
+function First(props)
+{
+  return(
+  <div>
+  <h1>hi</h1>
+  <Second name="harish"/>
+  </div>
+  );
+}
+
+function Second(props)
+{
+  let count=0;
+  return(
+  <div>
+  <h3>{count}</h3>
+  <h2>hello {props.name}</h2>
+  <Third />
+  </div>
+  );
+}
+
+class Third extends React.Component
+{
+  constructor()
+  {
+    super();
+    this.state={count:1};
+  }
+
+  handleIncrement =() =>
+  {
+    this.setState({count:this.state.count+1});
+  }
+
+  render()
+  {
+    return (
+      <div>
+      <h1>Vankam pa</h1>
+    <h2>{this.state.count}</h2>
+      <button onClick={this.handleIncrement}>CLICK</button>
+      
+      </div>
+    );
+
+  }
+}
+
+
+const element=(
+  <h2>heyy</h2>
+);
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <First />,
   document.getElementById('root')
 );
 
